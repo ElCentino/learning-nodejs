4add258b9791a78905624df8b78c4e7880d346bf
Century <favouronu@gmail.com>
2018-03-22 19:35:09 +0000

    Added filesystem for image files

--- fileserver.js
+++ fileserver.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 
 var dir = getArg("--dir");
+var imgDir = getArg("--imgdir");
 
 var index = dir.lastIndexOf("/");
 var pub = dir.substring(0, index);
@@ -13,16 +14,86 @@ function getArg(args) {
   return (index === -1) ? null : process.argv[index + 1];
 }
 
+function getFiles() {
+
+  var list;
+
+  fs.readdir(imgDir, function(err, files) {
+
+    if(err) throw err;
+
+    files.forEach(function(fileName) {
+      var file = path.join(imgDir, fileName);
+      var fileStat = fs.statSync(file);
+
+      console.log(file);
+      list += `<li><img src="${file}" alt="${fileName}"/></li>`;
+
+      if(fileStat.isFile() && file.endsWith(".png")) {
+
+      }
+    });
+  });
+
+  console.log(list);
+
+  return list;
+}
+
 var server = http.createServer(function(req, res) {
 
     console.log(`${req.port} || ${req.method} request for ${req.url}`);
 
     if(req.url === "/") {
 
-      fs.readFile(`${dir}/portfolio.html`, "UTF-8", function(err, html) {
-        res.writeHead(200, {"Content-Type" : "text/html"});
-        res.end(html);
-      });
+      // fs.readFile(`${dir}/portfolio.html`, "UTF-8", function(err, html) {
+      //   res.writeHead(200, {"Content-Type" : "text/html"});
+      //   res.end(html);
+      // });
+
+      res.writeHead(200, {"Content-Type" : "text/html"});
+      res.end(`
+        <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Port Folio</title>
+    <link type="text/css" rel="stylesheet" media="screen" href="css/portfolio.css" />
+      <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <h2>Portfolio</h2>
+    </header>
+
+    <div class="overlay">
+
+    </div>
+
+    <div id="frame">
+      <table id="frame_table">
+        <td id="left"> < </td>
+        <td id="right">></td>
+      </table>
+
+      <section id="info">
+        <p>Hello all</p>
+      </section>
+      <img id="main" alt="img" src="" />
+    </div>
+
+    <div id="wrapper">
+      <ul id="portfolio">
+        ${getFiles()}
+      </ul>
+    </div>
+
+    <script src="scripts/jquery-3.3.1.js" type="text/javascript"></script>
+    <script type="text/javascript" src="scripts/portfolio.js"></script>
+  </body>
+</html>
+      `);
 
     } else if(req.url.match(/.css$/)) {
 
